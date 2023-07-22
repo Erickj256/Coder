62b479c2b35a8cb4eb262d80fb14a2510dbe55fc
Erick Vergara <63665559+Erickj256@users.noreply.github.com>
2023-07-22 00:24:39 -0600

    Tabla en redshift
    Se agrega el pre de la tabla

---
+++ preTabla.sql
@@ -0,0 +1,21 @@
+create table josafat_math_coderhouse.clima_cdmx (
+	id varchar (50),
+	cityid varchar(50),
+	validdateutc varchar(50),
+	winddirectioncardinal varchar (25),
+	probabilityofprecip int,
+	relativehumidity int, 
+	name varchar(50),
+	date_insert varchar(60),
+	longitude int,
+	state varchar(50),
+	lastreporttime varchar(50),
+	skydescriptionlong varchar(50),
+	stateabbr varchar(25),
+	tempc int,
+	latitude int,
+	iconcode int,
+	windspeedkm int
+);
+
+select * from josafat_math_coderhouse.clima_cdmx;
